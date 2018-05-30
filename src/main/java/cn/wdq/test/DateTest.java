df6cf6b5bec6f5ce11fc5102d2223971c117df4d
coder_waldon <30494925+qq852727515@users.noreply.github.com>
2018-05-30 10:17:03 +0800

    Update DateTest.java

--- src/main/java/cn/wdq/test/DateTest.java
+++ src/main/java/cn/wdq/test/DateTest.java
@@ -12,7 +12,10 @@ public class DateTest {
     /**
      * 试试github的更新
      * */
-    
+    @BeforeClass
+    public static void sysOut(){
+        System.out.println("试试");
+    }
     @Test
     public void dateTest(){
         String testDate="2015-12-09 00:00:05";
